742c9de38c3d1973b4c67868f03ed2a3c8dfe472
Satria <satria.ihza@brank.as>
2023-04-20 02:19:11 +0700

    integration register controller with service , and add scenario on utest

--- src/main/java/com/dsp/test/controller/RegisterController.java
+++ src/main/java/com/dsp/test/controller/RegisterController.java
@@ -1,12 +1,20 @@
 package com.dsp.test.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dsp.test.model.CommonError;
 import com.dsp.test.model.UserRegistrationRequest;
+import com.dsp.test.service.RegisterService;
 
 import jakarta.validation.Valid;
 
@@ -14,8 +22,21 @@ import jakarta.validation.Valid;
 @RequestMapping("/register")
 public class RegisterController {
 
+	private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
+
+	@Autowired
+	RegisterService registerService;
+
 	@PostMapping
-	public ResponseEntity<Void> registerUser(@RequestBody @Valid UserRegistrationRequest request) {
+	public ResponseEntity<?> registerUser(@RequestBody @Valid UserRegistrationRequest request) {
+		try {
+			registerService.register(request);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			CommonError responseBody = new CommonError("failed registration",
+					new ArrayList<>(Arrays.asList(e.getMessage())));
+			return ResponseEntity.badRequest().body(responseBody);
+		}
 		return ResponseEntity.ok().build();
 	}
 
--- src/test/java/com/dsp/test/controller/RegisterControllerTest.java
+++ src/test/java/com/dsp/test/controller/RegisterControllerTest.java
@@ -1,6 +1,7 @@
 package com.dsp.test.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,16 +11,22 @@ import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.dsp.test.model.CommonError;
 import com.dsp.test.model.UserRegistrationRequest;
+import com.dsp.test.service.RegisterService;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
@@ -31,39 +38,50 @@ import lombok.Setter;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class RegisterControllerTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(RegisterControllerTest.class);
 
-	@Autowired
+	@InjectMocks
 	private RegisterController controller;
+
+	@Mock
+	private RegisterService registerService;
 
 	private static final String name_exceed_limit = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 	@Test
-	public void testRegister() {
+	public void testRegister() throws Exception {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		List<RegisterScenario> sc = new ArrayList<>();
 		// scenario 1
 		sc.add(this.setScenario("success", new UserRegistrationRequest("081234567890", "name", "Abcd1234"),
-				new ArrayList<>(), HttpStatus.OK));
+				new ArrayList<>(), HttpStatus.OK, null));
 		// scenario 2
+		sc.add(this.setScenario("should return error phone already exists",
+				new UserRegistrationRequest("089999999999", "name", "Abcd1234"), new ArrayList<>(),
+				HttpStatus.BAD_REQUEST, "Phone Number is already exists"));
+		// scenario 3
 		sc.add(this.setScenario("invalid phone", new UserRegistrationRequest("a", "name", "Abcd1234"),
 				new ArrayList<>(Arrays.asList("phoneNumber-must start with '08' and contain only digits",
 						"phoneNumber-size must be between 10 and 13")),
-				HttpStatus.OK));
-		// scenario 3
+				HttpStatus.OK, null));
+		// scenario 4
 		sc.add(this.setScenario("name exceeded limit",
 				new UserRegistrationRequest("081234567890", name_exceed_limit, "Abcd1234"),
-				new ArrayList<>(Arrays.asList("name-size must be between 0 and 60")), HttpStatus.OK));
-		// scenario 4
+				new ArrayList<>(Arrays.asList("name-size must be between 0 and 60")), HttpStatus.OK, null));
+		// scenario 5
 		sc.add(this.setScenario("invalid password", new UserRegistrationRequest("081234567890", "name", "a"),
 				new ArrayList<>(Arrays.asList("password-must contains at least 1 capital letter and 1 number",
 						"password-size must be between 6 and 16")),
-				HttpStatus.OK));
+				HttpStatus.OK, null));
 
 		for (RegisterScenario r : sc) {
+			if (r.getRequest().getPhoneNumber().equals("089999999999")) {
+				doThrow(new Exception("Phone Number is already exists")).when(registerService).register(Mockito.any());
+			}
 			// throw the AssertionError to mark the test case as failed
 			try {
 				Set<ConstraintViolation<UserRegistrationRequest>> finalResult = validator.validate(r.getRequest());
@@ -81,8 +99,12 @@ public class RegisterControllerTest {
 					assertEquals(r.getErrorValidation(), errorResult);
 				}
 				if (r.getErrorValidation().size() == 0) {
-					ResponseEntity<Void> resp = controller.registerUser(r.getRequest());
+					ResponseEntity<?> resp = controller.registerUser(r.getRequest());
 					assertEquals(r.getExpectedStatusCode(), resp.getStatusCode());
+					if (r.getErrorService() != null) {
+						CommonError commonError = (CommonError) resp.getBody();
+						assertEquals(r.getErrorService(), commonError.getError().get(0));
+					}
 				}
 				logger.info("test success :" + r.getDescription());
 			} catch (AssertionError e) {
@@ -94,8 +116,8 @@ public class RegisterControllerTest {
 	}
 
 	private RegisterScenario setScenario(String desc, UserRegistrationRequest req, List<String> errorValidation,
-			HttpStatusCode expected) {
-		return new RegisterScenario(errorValidation, desc, req, expected);
+			HttpStatusCode expected, String errorService) {
+		return new RegisterScenario(errorValidation, desc, req, expected, errorService);
 	}
 }
 
@@ -107,4 +129,5 @@ class RegisterScenario {
 	private String description;
 	private UserRegistrationRequest request;
 	private HttpStatusCode expectedStatusCode;
+	private String errorService;
 }
