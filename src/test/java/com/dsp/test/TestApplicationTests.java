237616ad9bef66db7e3b37a8ed96cebdc68388b4
Satria <satria.ihza@brank.as>
2023-04-19 19:49:06 +0700

    unit test init - setup db config value for unit test - add h2 so we can use it as database for unit test

--- src/test/java/com/dsp/test/TestApplicationTests.java
+++ src/test/java/com/dsp/test/TestApplicationTests.java
@@ -2,8 +2,10 @@ package com.dsp.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TestApplicationTests {
 
 	@Test
